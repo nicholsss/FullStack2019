1a15bf6195acb688b42cff199da76ef14a2f6e89
nicholsss <mickyaw95@gmail.com>
2019-04-03 12:41:18 +0300

    make it better

--- notebackend/index.js
+++ notebackend/index.js
@@ -91,7 +91,7 @@ const error = (request, response) => {
 
 app.use(error)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
