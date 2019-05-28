6a3ad10f8b3d601f7ee37c03e07649908bdab05c
nicholsss <mickyaw95@gmail.com>
2019-05-28 11:06:47 +0300

    m

---
+++ osa6/unicafe-redux/src/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+};
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "GOOD":
+      console.log(state.good);
+      return { ...state, good: state.good + 1 };
+    case "OK":
+      return { ...state, ok: state.ok + 1 };
+    case "BAD":
+      return { ...state, bad: state.bad + 1 };
+    case "ZERO":
+      return {
+        ...state,
+        good: (state.good = 0),
+        ok: (state.ok = 0),
+        bad: (state.bad = 0)
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
