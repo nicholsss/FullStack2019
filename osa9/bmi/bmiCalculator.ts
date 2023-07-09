e0429d69440e510e71162ced92a6ac87df47aed6
Nicholas <mickyaw1995@gmail.com>
2023-07-09 23:30:22 +0300

    9.1 fixed and done

--- osa9/bmi/bmiCalculator.ts
+++ osa9/bmi/bmiCalculator.ts
@@ -1,5 +1,13 @@
 const calculateBmi = (height: number, weight: number)  => {
-    return 'Normal (healthy weight)'
+    const bmiHeight = height /100;
+    const bmi = weight / (bmiHeight ** 2)
+    if (bmi < 18.5) {
+        return 'Underweight';
+      } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)';
+      } else {
+        return 'Overweight';
+      }
 }
 
 console.log(calculateBmi(180, 74))
