5b0dbf99720df6cd50ab7ead56c8d9c6aceef6ee
Nicholas <mickyaw1995@gmail.com>
2024-04-19 13:26:41 +0300

    8.4 done

--- osa8/bookstore/library-backend.js
+++ osa8/bookstore/library-backend.js
@@ -117,7 +117,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
   
@@ -127,7 +127,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root,args) => books.filter(book => book.author === args.author),
     allAuthors: () => authors.map(author => ({
       ...author,
       bookCount: books.filter(book => book.author === author.name).length
